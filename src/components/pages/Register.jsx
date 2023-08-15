8dfd81707f9dcf76c5e6b184773749e337ebc08e
jtw007 <joshwu095@gmail.com>
2023-08-14 20:19:53 -0700

    removed commented out code

--- src/components/pages/Register.jsx
+++ src/components/pages/Register.jsx
@@ -130,17 +130,19 @@ const Register = ({ currentUser, setCurrentUser }) => {
                 <div style={{width: 89, height: 17, left: 0, top: 0, position: 'absolute', background: 'white'}} />
                 <div style={{width: 82.50, height: 17.83, left: 0, top: 0.38, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 12, fontFamily: 'Roboto', fontWeight: '400', letterSpacing: 0.20, wordWrap: 'break-word'}}>Email Address</div>
             </div>
+            {/* check box for the terms and conditions */}
+            <div style={{width: 11.06, height: 11.06, left: 93, top: 463.83, position: 'absolute', background: '#D9D9D9', borderRadius: 2, border: '1px solid'}} />
 
             <div style={{width: 224, height: 18, left: 66, top: 461, position: 'absolute', textAlign: 'right'}}>
+                
                 <span style={{color: '#5F6368', fontSize: 12, fontFamily: 'Roboto', fontWeight: '400', letterSpacing: 0.20, wordWrap: 'break-word'}}>Agree with </span>
                 <span style={{color: 'black', fontSize: 12, fontFamily: 'Roboto', fontWeight: '700', letterSpacing: 0.20, wordWrap: 'break-word'}}>Terms & Conditions</span>
             </div>
-
-            <div style={{width: 11.06, height: 11.06, left: 93, top: 463.83, position: 'absolute', background: '#D9D9D9', borderRadius: 2, border: '0.50px #5F6368 solid'}} />
+            
             <div style={{width: 256, height: 54, left: 83, top: 492, position: 'absolute', background: '#D9D9D9', borderRadius: 40, border: '1px rgba(95, 99, 104, 0.50) solid'}} />
-            <button 
+            <div 
                 style={{width: 216.66, height: 17.83, left: 103, top: 510.08, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 16, fontFamily: 'Roboto', fontWeight: '400', letterSpacing: 0.20, wordWrap: 'break-word'}}>Create Account
-            </button>
+            </div>
 
             <div style={{width: 295.02, height: 17.83, left: 59, top: 568, position: 'absolute', textAlign: 'center'}}>
                 <span style={{color: '#5F6368', fontSize: 12, fontFamily: 'Roboto', fontWeight: '400', letterSpacing: 0.20, wordWrap: 'break-word'}}>Already have an account?</span>
@@ -154,78 +156,6 @@ const Register = ({ currentUser, setCurrentUser }) => {
             <div style={{width: 216.66, height: 17.83, left: 128, top: 666, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 16, fontFamily: 'Roboto', fontWeight: '400', letterSpacing: 0.20, wordWrap: 'break-word'}}>Continue with Google</div>
             <div style={{width: 252, height: 0, left: 87, top: 608, position: 'absolute', border: '1.50px #AFB1B3 solid'}}></div>
             <div style={{width: 26, height: 26, left: 106.62, top: 675, position: 'absolute', transform: 'rotate(-45deg)', transformOrigin: '0 0', background: '#D9D9D9', borderRadius: 9999}} />
-            {/* <div style={{width: 26, height: 0, left: 116.51, top: 684.90, position: 'absolute', transform: 'rotate(-45deg)', transformOrigin: '0 0', border: '0.50px black solid'}}></div>
-            <div style={{width: 26, height: 0, left: 115.81, top: 665.81, position: 'absolute', transform: 'rotate(45deg)', transformOrigin: '0 0', border: '0.50px black solid'}}></div> */}
-                
-                {/* <div style={{ width: 216.66, height: 17.83, left: 103, top: 34, position: 'relative', border: '1px solid red'}}>
-                    <input 
-                        style={{width: 256, height: 55, left: 0, top: 85, position: 'relative', borderRadius: 40, border: '1px rgba(95, 99, 104, 0.50) solid'}}
-                        type="text"
-                        id="name"
-                        placeholder='name'
-                        onChange={e => setName(e.target.value)}
-                        value={name}
-                        required
-                    />
-                    <div style={{width: '100%', height: '100%', textAlign: 'center', color: 'black', fontSize: 12, fontFamily: 'Roboto', fontWeight: '400', letterSpacing: 0.20, wordWrap: 'break-word'}}>Full Name</div>
-                </div>
-				
-                <div style={{width: 89, height: 18, left: 115, top: 230, position: 'relative'}}>
-                    <input 
-                        style={{width: 256, height: 55, left: 83, top: 83.56, position: 'relative', borderRadius: 40, border: '1px rgba(95, 99, 104, 0.50) solid'}}
-                        type="email"
-                        id="email"
-                        placeholder='email'
-                        onChange={e => setEmail(e.target.value)}
-                        value={email}
-                        required
-                    />
-                    <div style={{width: '100%', height: '100%', textAlign: 'center', color: 'black', fontSize: 12, fontFamily: 'Roboto', fontWeight: '400', letterSpacing: 0.20, wordWrap: 'break-word'}}>Email Address</div>
-                </div>
-                    
-                <div style={{width: 89, height: 18, left: 115, top: 230, position: 'relative'}}>
-                    <input 
-                        style={{width: 256, height: 55, left: 83, top: 83.56, position: 'relative', borderRadius: 40, border: '1px rgba(95, 99, 104, 0.50) solid'}}
-                        type="number"
-                        id="phoneNumber"
-                        placeholder='phone number'
-                        onChange={e => setPhoneNumber(e.target.value)}
-                        value={phoneNumber}
-                        required
-                    />
-                    <div style={{width: '100%', height: '100%', textAlign: 'center', color: 'black', fontSize: 12, fontFamily: 'Roboto', fontWeight: '400', letterSpacing: 0.20, wordWrap: 'break-word'}}>Phone Number</div>
-                </div>
-
-                <div style={{width: '100%', height: '100%', textAlign: 'center', color: 'black', fontSize: 12, fontFamily: 'Roboto', fontWeight: '400', letterSpacing: 0.20, wordWrap: 'break-word'}}>Password</div>
-				<input 
-                    style={{width: 256, height: 55, left: 83, top: 83.56, position: 'relative', borderRadius: 40, border: '1px rgba(95, 99, 104, 0.50) solid'}}
-					type="password"
-					id="password"
-					placeholder='password'
-					onChange={e => setPassword(e.target.value)}
-					value={password}
-					required
-				/>
-
-				<button 
-                    type="submit" 
-                    id='register-btn'
-                    style={{width: 256, height: 55, left: 83, top: 161.56, position: 'relative', borderRadius: 40, border: '1px rgba(95, 99, 104, 0.50) solid'}}
-                >Create Account</button>
-
-                <div style={{width: '100%', height: '100%', textAlign: 'center'}}>
-                    <span style={{color: '#5F6368', fontSize: 12, fontFamily: 'Roboto', fontWeight: '400', letterSpacing: 0.20, wordWrap: 'break-word'}}>Already have an account?</span>
-                    <span style={{color: 'black', fontSize: 12, fontFamily: 'Roboto', fontWeight: '400', letterSpacing: 0.20, wordWrap: 'break-word'}}> </span>
-                    <span style={{color: 'black', fontSize: 12, fontFamily: 'Roboto', fontWeight: '700', letterSpacing: 0.20, wordWrap: 'break-word'}}>Login</span>
-                </div>
-
-			</form>
-
-            <div style={{width: 295.02, height: 17.83, left: 59, top: 568, position: 'relative', textAlign: 'center'}}>
-                <span style={{color: '#5F6368', fontSize: 12, fontFamily: 'Roboto', fontWeight: '400', letterSpacing: 0.20, wordWrap: 'break-word'}}>Already have an account?</span>
-                <span style={{color: 'black', fontSize: 12, fontFamily: 'Roboto', fontWeight: '400', letterSpacing: 0.20, wordWrap: 'break-word'}}></span>
-                <span style={{color: 'black', fontSize: 12, fontFamily: 'Roboto', fontWeight: '700', letterSpacing: 0.20, wordWrap: 'break-word'}}>Login</span>
-            </div> */}
             
 		</div>
     )
