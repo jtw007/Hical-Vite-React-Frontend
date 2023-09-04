9b17d4b86aeab5becd92a5bd6d90c43497fb45ed
jtw007 <joshwu095@gmail.com>
2023-09-04 16:13:50 -0700

    finished input form for create activity component

--- src/components/pages/CreateActivity.jsx
+++ src/components/pages/CreateActivity.jsx
@@ -11,9 +11,47 @@ const CreateActivity = () => {
         participants: '',
         user: '',
     })
+    const [activity, setActivity] = useState('')
+    // invoke useNavigate hook to get a navigate function to use
+    const navigate = useNavigate()
 
+    //localStorage = web storage object that allows JS sites and apps to keep key-value pairs in web browser with no expiration date; enables developers to store and retrieve data in the browser - not good practice since data will be lost if the user clears cache
+        //in this case, we are storing the jwt 
+    // const token = localStorage.getItem('jwt')
+    // if(!token) {
+    //     return <Navigate to="/login" />
+    // }
+    // decodes the token
+    // const decoded = jwt_decode(token)
+    // console.log(decoded.id)
+    //submit handler function that posts the form data from state to the backend
+    const handleSubmit = (e) => {
+        e.preventDefault()
+    }
+        
+    
     return (
-        <div>CreateActivity</div>
+        <div className='activity-container'>
+            <div id='activity-message'>
+                <p style={{width: 257, height: 18, left: 46.13, top: 48.54, position: 'absolute', color: '#4B444D', fontSize: 24, fontFamily: 'Roboto', fontWeight: '400', letterSpacing: 0.20, wordWrap: 'break-word'}}>Create New Activity</p>
+            </div>
+
+            <form onSubmit={handleSubmit}>
+                <div className='form-container'>
+                    <div className=''>
+                        <label htmlFor='activity-title'>Activity Title</label>
+                        <input 
+                            type='text'
+                            id='name'
+                            placeholder='Activity Title'
+                            onChange={e => setActivity(e.target.value)}
+                            value={activity}
+                            required
+                        />
+                    </div>
+                </div>
+            </form>
+        </div>
     )
 }
 
